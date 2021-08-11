22d307a9c297814093faf756e3d4360c1a23c82b
Dan Seol <yunheum.seol@mail.mcgill.ca>
2021-08-10 22:42:26 -0400

    io and exceptions

---
+++ csharp/W3Schools/io_and_exceptions/Determinant.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace linalg {
+
+	public class Determinant
+	{
+		private double[,] matrix;
+
+		public Determinant(double[,] matrix)
+		{
+			this.matrix = matrix;
+		}
+
+		public double det() 
+		{
+			return determinant(this.matrix);
+		}
+		
+		private double determinant(double[,] A)
+		{
+			int m = A.GetLength(0);
+			int n = A.GetLength(1);
+			if (m != n)
+			{
+				throw new ArithmeticException("The matrix is not a square matrix");
+			}
+
+			if (n == 1)
+			{
+				return A[0, 0];
+			}
+			else if (n == 2) 
+			{
+				return A[0, 0] * A[1, 1] - A[0, 1] * A[1, 0];
+			} 
+			else
+            {
+				double det = 0;
+				for (int i = 0; i < n; i++) {
+					double[,] minor = getMinor(A, i, 0);
+					det += cofactorSign(i, 0) * A[i, 0] * determinant(minor);
+				}
+
+				return det;
+            }
+		}
+
+		private static int cofactorSign(int i, int j)
+		{
+			return ((i + j) % 2 == 0) ? 1 : -1;
+		}
+
+		private double[,] getMinor(double[,] A, int i, int j)
+		{
+			int m = A.GetLength(0);
+			int n = A.GetLength(1);
+			if (m != n)
+			{
+				throw new ArithmeticException("The matrix is not a square matrix");
+
+			}
+			double[,] minor = new double[n - 1, n - 1];
+			for (int k = 0; k < n; k++) 
+			{
+				if (k != i) 
+				{
+					for (int l = 0; l < n; l++)
+					{
+						if (l != j) 
+						{
+							int k1 = (k < i) ? k : k - 1;
+							int l1 = (l < j) ? l : l - 1;
+							minor[k1, l1] = A[k, l];
+						}
+						
+					}
+				}
+				
+			}
+
+			return minor;
+		}
+	}
+
+}
---
+++ csharp/W3Schools/io_and_exceptions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+namespace linalg {
+	public class Program
+	{
+		static void Main()
+		{
+			string invertibleMatRaw = File.ReadAllText("../../../matrixInvertible.csv");
+			string singularMatRaw = File.ReadAllText("../../../matrixSingular.csv");
+			string[] invertibleMatLines = invertibleMatRaw.Split("\n");
+			string[] singularMatLines = singularMatRaw.Split("\n");
+
+			int invertibleMatDim = invertibleMatLines.Length;
+			int singularMatDim = singularMatLines.Length;
+
+			double[,] invertibleMat = new double[invertibleMatDim, invertibleMatDim];
+			double[,] singularMat = new double[singularMatDim, singularMatDim];
+
+			for (int i = 0; i < invertibleMatDim; i++) 
+			{
+				string[] invertibleMatLine = invertibleMatLines[i].Split(",");
+				for (int j = 0; j < invertibleMatLine.Length; j++) 
+				{
+					invertibleMat[i, j] = Convert.ToDouble(invertibleMatLine[j]);
+				}
+			}
+
+			for (int i = 0; i < singularMatDim; i++)
+			{
+				string[] singularMatLine = singularMatLines[i].Split(",");
+				for (int j = 0; j < singularMatLine.Length; j++)
+				{
+					singularMat[i, j] = Convert.ToDouble(singularMatLine[j]);
+				}
+			}
+
+			Determinant det1 = new Determinant(invertibleMat);
+			Determinant det2 = new Determinant(singularMat);
+
+			double d1 = det1.det();
+			double d2 = det2.det();
+
+			Console.WriteLine($"For invertible matrix the determinant is {d1}");
+			Console.WriteLine($"For singular matrix the determinant is {d2}");
+		}
+	}
+}
+
